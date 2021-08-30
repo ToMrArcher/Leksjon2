a1836e77200f076ef6190676fee4ca43d82f28ab
syverteidjord <syvertcollege>
2021-08-30 19:04:42 +0200

    Test fails

--- src/test/java/com/eidjord/romanTest.java
+++ src/test/java/com/eidjord/romanTest.java
@@ -1,5 +1,17 @@
 package com.eidjord;
 
-public class romanTest {
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class romanTest {
+    @Test
+    void shouldConvert1ToI() {
+        assertEquals("I", toRoman(1));
+    }
+
+    private String toRoman(int i) {
+        return null;
+    }
 }
