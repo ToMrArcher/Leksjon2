91e009888e373b9b7f848e6d51d3e42d192f79a4
syverteidjord <syvertcollege>
2021-08-31 12:00:18 +0200

    Refactor

--- src/main/java/com/eidjord/romanConverter.java
+++ src/main/java/com/eidjord/romanConverter.java
@@ -13,25 +13,4 @@ public class romanConverter {
         }
         return romanNumber;
     }
-
-    public String perfectConvert(int i){
-        switch(i){
-            case 1000:
-                return "M";
-            case 500:
-                return "D";
-            case 100:
-                return "C";
-            case 50:
-                return "L";
-            case 10:
-                return "X";
-            case 5:
-                return "V";
-            case 1:
-                return "I";
-            default:
-                throw new IllegalArgumentException();
-        }
-    }
 }
